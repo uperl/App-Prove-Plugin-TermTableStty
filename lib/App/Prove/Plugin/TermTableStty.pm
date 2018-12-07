5b242453d188ae4bb571cc5b90b96ba43bdac0fa
Graham Ollis <plicease@cpan.org>
2018-12-06 20:18:05 -0500

    initial implementation

--- lib/App/Prove/Plugin/TermTableStty.pm
+++ lib/App/Prove/Plugin/TermTableStty.pm
@@ -7,4 +7,50 @@ use 5.008001;
 # ABSTRACT: Set the size of the console for Term::Table using stty size
 # VERSION
 
+=head1 SYNOPSIS
+
+From command-line:
+
+ prove -PTermTable
+
+From .proverc:
+
+ -PTermTable
+
+=head1 DESCRIPTION
+
+Some of the L<Test2::Tools> provide some pretty tables for comparing deltas from failed tests.  It tries to 
+detect the size of the terminal so that long lines can be wrapped and the tables remain readable.  Unfortunately
+when you run the tests under C<prove>, C<stdout> is redirected to a non-terminal and the size of the terminal
+cannot be detected.  This plugin will detect the size of the terminal using C</dev/tty> instead of C<stdout> and
+set the appropriate environment variable so that tables can be printed to use the entire terminal diameter.
+
+I understand why 80 columns is the default for when you do not know the size of a terminal, but really who here 
+in 2018 is actually using an 80 column display?  sigh.
+
+This module is similar to L<App::Prove::Plugin::TermTableStty> but has fewer prereqs and requires a platform that
+supports the C<stty size> command.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Test2>
+
+=item L<App::Prove>
+
+=back
+
+=cut
+
+sub load
+{
+  return if $ENV{TERM_TABLE_SIZE};
+  my $size = `stty size`;
+  if($size =~ /[0-9]+\s+([0-9]+)/)
+  {
+    $ENV{TERM_TABLE_SIZE} = $1;
+  }
+}
+
 1;
--- t/app_prove_plugin_termtablestty.t
+++ t/app_prove_plugin_termtablestty.t
@@ -1,6 +1,24 @@
 use Test2::V0 -no_srand => 1;
 use App::Prove::Plugin::TermTableStty;
 
-ok 1, 'todo';
+subtest 'already set' => sub {
+
+  local $ENV{TABLE_TERM_SIZE} = 9999;
+  App::Prove::Plugin::TermTableStty->load;
+  is $ENV{TABLE_TERM_SIZE}, 9999;
+
+};
+
+subtest 'not already set' => sub {
+
+  local $ENV{TERM_TABLE_SIZE};
+  delete $ENV{TERM_TABLE_SIZE};
+  
+  App::Prove::Plugin::TermTableStty->load;
+  is $ENV{TERM_TABLE_SIZE}, match qr/^[0-9]+$/;
+  
+  note "TERM_TABLE_SIZE=$ENV{TERM_TABLE_SIZE}";
+
+};
 
 done_testing
