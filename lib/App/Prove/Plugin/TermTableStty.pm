a0c6805bcd6cea61138b824c916618401f399878
Graham Ollis <plicease@cpan.org>
2018-12-06 20:06:42 -0500

    Initial commit

---
+++ lib/App/Prove/Plugin/TermTableStty.pm
@@ -0,0 +1,10 @@
+package App::Prove::Plugin::TermTableStty;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Set the size of the console for Term::Table using stty size
+# VERSION
+
+1;
---
+++ t/app_prove_plugin_termtablestty.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use App::Prove::Plugin::TermTableStty;
+
+ok 1, 'todo';
+
+done_testing
